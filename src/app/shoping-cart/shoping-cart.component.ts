d160b97f574673ff1ecf95b2614d67681b89b6b2
Francoviera <vierafranco1@gmail.com>
2020-10-19 21:43:44 -0300

    se creo la vista del carrito

--- src/app/shoping-cart/shoping-cart.component.ts
+++ src/app/shoping-cart/shoping-cart.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Product } from '../products/product';
 @Component({
   selector: 'app-shoping-cart',
   templateUrl: './shoping-cart.component.html',
@@ -8,6 +8,30 @@ import { Component, OnInit } from '@angular/core';
 export class ShopingCartComponent implements OnInit {
 
   constructor() { }
+
+  products: Product[] = [
+    {
+      name: "coca-cola",
+      price: 200,
+      img: "assets/img/producto.jpg",
+      ofert: true,
+      stock: 30
+    },
+    {
+      name: "Pan",
+      price: 80,
+      img: "assets/img/producto.jpg",
+      ofert: false,
+      stock: 0
+    },
+    {
+      name: "Fernet",
+      price: 500,
+      img: "assets/img/producto.jpg",
+      ofert: true,
+      stock: 30
+    }
+  ]
 
   ngOnInit(): void {
   }
